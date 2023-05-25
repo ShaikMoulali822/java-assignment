614666352f1a728820cbd3cbdf1368ae1a8ee73f
ShaikMoulali822 <smoulali761@gmail.com>
2023-05-25 10:21:39 +0530

    main9

---
+++ main9.java
@@ -0,0 +1,11 @@
+package Moulali;
+public class main9 {
+
+    public static void main(String[] args) {
+
+        IDandPasswords idandPasswords = new IDandPasswords();
+
+        LoginPage loginPage = new LoginPage(idandPasswords.getLoginInfo());
+
+    }
+}
